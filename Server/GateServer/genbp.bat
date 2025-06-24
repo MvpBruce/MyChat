35e93df97bcb0f0db085cf9c5f262791446b83c4
Bruce <Bruce@gmail.com>
2025-06-24 01:40:43 -0400

    add grpc genarate scripts

---
+++ Server/GateServer/genbp.bat
@@ -0,0 +1,2 @@
+E:\grpc\visualpro\third_party\protobuf\Debug\protoc.exe --cpp_out=. "message.proto"
+pause
---
+++ Server/GateServer/gengrpc.bat
@@ -0,0 +1,2 @@
+E:\grpc\visualpro\third_party\protobuf\Debug\protoc.exe  -I="." --grpc_out="." --plugin=protoc-gen-grpc="E:\grpc\visualpro\Debug\grpc_cpp_plugin.exe" "message.proto"
+pause
