639ea13cdaa660559b268487c414675fbc6598bf
Bruce <Bruce@gmail.com>
2025-07-22 23:22:14 -0400

    add sql

---
+++ sql/mychat.sql
@@ -0,0 +1,74 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `uid` int NOT NULL DEFAULT 0,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `pwd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uid`(`uid` ASC) USING BTREE,
+  INDEX `name`(`name` ASC) USING BTREE,
+  UNIQUE INDEX `email`(`email` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (2, 1001, 'bruce', '300200@qq.com', '123456');
+
+-- ----------------------------
+-- Table structure for user_id
+-- ----------------------------
+DROP TABLE IF EXISTS `user_id`;
+CREATE TABLE `user_id`  (
+  `id` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of user_id
+-- ----------------------------
+INSERT INTO `user_id` VALUES (1001);
+
+-- ----------------------------
+-- Procedure structure for reg_user
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `reg_user`;
+delimiter ;;
+CREATE PROCEDURE `reg_user`(IN `new_name` VARCHAR(255), 
+    IN `new_email` VARCHAR(255), 
+    IN `new_pwd` VARCHAR(255), 
+    OUT `result` INT)
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        ROLLBACK;
+        SET result = -1;
+    END;
+    START TRANSACTION;
+    IF EXISTS (SELECT 1 FROM `user` WHERE `name` = new_name) THEN
+        SET result = 0;
+        COMMIT;
+    ELSE
+        IF EXISTS (SELECT 1 FROM `user` WHERE `email` = new_email) THEN
+            SET result = 0;
+            COMMIT;
+        ELSE
+            UPDATE `user_id` SET `id` = `id` + 1;
+            SELECT `id` INTO @new_id FROM `user_id`;
+            INSERT INTO `user` (`uid`, `name`, `email`, `pwd`) VALUES (@new_id, new_name, new_email, new_pwd);
+            SET result = @new_id;
+            COMMIT;
+        END IF;
+    END IF;
+END
+;;
+delimiter ;
+
+SET FOREIGN_KEY_CHECKS = 1;
